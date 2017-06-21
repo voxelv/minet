c538f8645abc409588f9ffd9af6e43520d12cc02
Tim Slippy, voxelv <slippytim@gmail.com>
2017-06-20 20:34:52 -0700

    hey it works

--- main.py
+++ main.py
@@ -13,15 +13,6 @@ class Lot(object):
         self.tag = tag
         self.amt = amt
 
-    def __eq__(self, other):
-        return other.tag == self.tag and other.amt == self.amt
-
-    def __ne__(self, other):
-        return not (self == other)
-
-    def __hash__(self):
-        return hash((self.tag, self.amt))
-
     def __repr__(self):
         return ".{} {}.".format(self.tag, self.amt)
 
@@ -31,12 +22,14 @@ class Slot(object):
     Contains a Lot, is part of a Unit
     """
 
-    def __init__(self, unit, tag):
+    def __init__(self, unit, tag, rcp_amt):
         self.unit = unit if isinstance(unit, Unit) else None
         self.tag = tag
         self.lot = None
 
         self.flow = "NONE"
+
+        self.rcp_amt = rcp_amt
 
         self.src_slots = []
         self.tgt_slots = []
@@ -49,8 +42,6 @@ class Slot(object):
 
         if self.lot is None:
             self.lot = lot
-        elif lot.tag == self.lot.tag and lot.amt > 0:
-            self.lot.amt += lot.amt
 
     def take(self):
         if self.lot is not None:
@@ -85,7 +76,6 @@ class SlotMgr(object):
         elif isinstance(slot, RxSlot) and slot not in SlotMgr.rx_slots:
             SlotMgr.rx_slots.append(slot)
 
-
     @staticmethod
     def tick(tick):
         SlotMgr.on_tick = tick
@@ -100,15 +90,25 @@ class SlotMgr(object):
                 div_amt = int(floor(lot.amt / len(slot.tgt_slots)))
                 # Give to targets
                 for rx_slot in slot.tgt_slots:
-                    rx_slot.give(Lot(slot.tag, div_amt))
-                # Keep the remaining amount_
+                    rx_lot = rx_slot.take()
+                    # Create lots if needed
+                    if rx_lot is None:
+                        rx_lot = Lot(slot.tag, 0)
+                    # Increment lot amt
+                    rx_lot.amt += div_amt
+                    # Give the lot to the rx slot
+                    rx_slot.give(rx_lot)
+
+                # Keep the remaining amount
                 rem_amt = lot.amt % len(slot.tgt_slots)
-                slot.give(Lot(slot.tag, rem_amt))
+                # Destroy lots if no longer needed
+                if rem_amt > 0:
+                    slot.give(Lot(slot.tag, rem_amt))
 
 
 class RxSlot(Slot):
-    def __init__(self, unit, rcp):
-        super(RxSlot, self).__init__(unit, rcp)
+    def __init__(self, unit, rcp, rcp_amt):
+        super(RxSlot, self).__init__(unit, rcp, rcp_amt)
         self.flow = "IN"
 
     def connect_to(self, slot):
@@ -121,8 +121,8 @@ class RxSlot(Slot):
 
 
 class TxSlot(Slot):
-    def __init__(self, unit, rcp):
-        super(TxSlot, self).__init__(unit, rcp)
+    def __init__(self, unit, rcp, rcp_amt):
+        super(TxSlot, self).__init__(unit, rcp, rcp_amt)
         self.flow = "OUT"
 
     def connect_to(self, slot):
@@ -134,46 +134,92 @@ class TxSlot(Slot):
         return False
 
 
+class UnitMgr(object):
+    units = []
+
+    @staticmethod
+    def add_unit(unit):
+        if unit not in UnitMgr.units:
+            UnitMgr.units.append(unit)
+
+    @staticmethod
+    def tick(tick):
+        for unit in UnitMgr.units:
+            unit.tick(tick)
+
+
 class UnitRcp(object):
     def __init__(self, req_lots, mak_lots):
         self.req_lots = req_lots
         self.mak_lots = mak_lots
+
+    def __repr__(self):
+        return "{}->{}".format(self.req_lots, self.mak_lots)
 
 
 class Unit(object):
     def __init__(self, rcp):
         self.rcp = rcp if isinstance(rcp, UnitRcp) else None
 
-        self.rx_slots = {}
-        for idx, rcp_lot in enumerate(self.rcp.req_lots):
-            self.rx_slots[idx] = (RxSlot(self, rcp_lot.tag), rcp_lot)
+        self.rx_slots = []
+        for rcp_lot in self.rcp.req_lots:
+            self.rx_slots.append(RxSlot(self, rcp_lot.tag, rcp_lot.amt))
 
-        self.tx_slots = {}
-        for idx, rcp_lot in enumerate(self.rcp.mak_lots):
-            self.tx_slots[idx] = (TxSlot(self, rcp_lot.tag), rcp_lot)
+        self.tx_slots = []
+        for rcp_lot in self.rcp.mak_lots:
+            self.tx_slots.append(TxSlot(self, rcp_lot.tag, rcp_lot.amt))
+
+        UnitMgr.add_unit(self)
 
     def tick(self, tick):
         # Start by assuming we have all the lots we need to do our recipe
         lots_avbl = True
         # Now if any is insufficient, then lots are not avbl
-        for rcp_lot, slot in self.rx_slots.iteritems():
-            if slot.lot.amt < rcp_lot.amt:
+        for slot in self.rx_slots:
+            if slot.lot is None:
+                return
+
+            if slot.lot.amt < slot.rcp_amt:
                 lots_avbl = False
 
         # If we have enough, do our recipe
         if lots_avbl:
             # Nom the rx
-            for slot_info in self.rx_slots.itervalues():
-                slot_info[0].lot.amt -= slot_info[1].amt
+            for slot in self.rx_slots:
+                lot = slot.take()
+                lot.amt -= slot.rcp_amt
+                if lot.amt > 0:
+                    slot.give(lot)
 
             # Create the tx
-            for slot_info in self.tx_slots.itervalues():
-                slot_info[0].lot.amt += slot_info[1].amt
+            for slot in self.tx_slots:
+                lot = slot.take()
+                if lot is None:
+                    lot = Lot(slot.tag, 0)
+                lot.amt += slot.rcp_amt
+                slot.give(lot)
+
+    def __repr__(self):
+        return "Unit: {}->{}".format(self.rcp.req_lots, self.rcp.mak_lots)
 
 
 def run():
     print "K GO"
     boiler = Unit(UnitRcp([Lot("COAL", 1), Lot("WATER", 10)], [Lot("STEAM", 100)]))
+    coal_src = Unit(UnitRcp([], [Lot("COAL", 2)]))
+    water_src = Unit(UnitRcp([], [Lot("WATER", 10)]))
+    steam_sink = Unit(UnitRcp([Lot("STEAM", 100)], []))
+
+    boiler.rx_slots[0].connect_to(coal_src.tx_slots[0])
+    boiler.rx_slots[1].connect_to(water_src.tx_slots[0])
+    boiler.tx_slots[0].connect_to(steam_sink.rx_slots[0])
+
+    tick = 0
+    done = False
+    while not done:
+        SlotMgr.tick(tick)
+        UnitMgr.tick(tick)
+        tick += 1
 
     print "K DONE"
 
